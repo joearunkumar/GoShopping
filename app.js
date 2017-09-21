04ac02def25c94425ed9e50ec73d19c5b368bd62
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-21 19:14:53 +0530

    getting response using apply

--- app.js
+++ app.js
@@ -274,7 +274,8 @@ function receivedMessage(event) {
       console.log("processToDo: ",processToDo);
 
       if(processToDo == "function") {
-        eval(array[1])(senderID);
+        //eval(array[1])(senderID);
+        apply(array[1],senderID);
       } else if(processToDo == "text") {
         sendTextMessage(senderID, array[1]);
       }
