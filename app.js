be1e0f4f242a4ff7e486f242fd619cb7d35bf88c
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-22 13:53:15 +0530

    getstarted button1

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  setupGetStartedButton(res);
+  //setupGetStartedButton(res);
   console.log("data: ", data)
   // Make sure this is a page subscription
   if (data.object == 'page') {
