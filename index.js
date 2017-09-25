790b1f514e4c3322424519ebe7b846ced238f3f3
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-25 15:37:00 +0530

    setup fullfillment7

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static('public'));
 app.post('/helloHttp', function(req, res) {
 //exports.helloHttp = function helloHttp (req, res) {
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-
+  console.log("Request: ",req);
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ "speech": response, "displayText": response
   //"speech" is the spoken version of the response, "displayText" is the visual version
