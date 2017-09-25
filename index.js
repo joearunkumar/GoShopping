a77c0beb25369a1886d3a684b51082adb5241452
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-25 15:08:04 +0530

    setup fullfillment5

--- index.js
+++ index.js
@@ -27,3 +27,12 @@ app.get('/helloHttp', function(req, res) {
   //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
 });
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
