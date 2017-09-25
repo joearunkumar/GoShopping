a0b8ed15b83bb15f91c3fd98fb4bc365f88ff7fa
598185 <a598185@TCSC02SK6E8G8WN.local>
2017-09-25 15:05:39 +0530

    setup fullfillment4

---
+++ index.js
@@ -0,0 +1,29 @@
+const
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),
+  request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+//app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+/*
+* HTTP Cloud Function.
+*
+* @param {Object} req Cloud Function request context.
+* @param {Object} res Cloud Function response context.
+*/
+app.get('/helloHttp', function(req, res) {
+//exports.helloHttp = function helloHttp (req, res) {
+  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+});
